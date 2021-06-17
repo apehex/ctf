d5b8613c6412ee48eef755c5f3948098f59a56fa
apehex <apehex@protonmail.com>
2021-06-17 19:32:50 +0000

    Document the algorithm behind the custom padding oracle attack (HTB fibopadcci)

--- hackthebox/crypto/fibopadcci/README.md
+++ hackthebox/crypto/fibopadcci/README.md
@@ -7,11 +7,12 @@
 ## Interacting with the server
 
 The server has 2 modes:
+
 - mode 0 sends the flag
 - mode 1 sends a message
 
 Each time you request the encryption the result changes. Supposing that the flag
-is constant, the key is somehow randomized.
+and key are constant, the change comes from the randomization of the IV a & b.
 
 ```
 encrypted_flag: 47b756c88a143410ed2ba29e94689b89fcba3706f14b33fbbf65be59b78c4a5845fc5c2a1bbb66a21f0b3b23dbeed815
@@ -19,14 +20,49 @@ a: b1d755cd8fff461835974451f7528fd3
 b: dcfefc222548071dc06db50c3b6e1e18
 ```
 
+In return, the mode 1 allow to send a ciphertext with its IV. The server's
+only only says whether the padding is correct after decryption.
+
 ## The scheme
 
 AES is used in block mode:
+
 - the block length is 16 bytes
-- the data is padded, to reach a length multiple of 16
+- the data is padded:
+  - until it reaches a length multiple of 16
+  - the padding values are an excerpt from the Fibonacci sequence
 - then it is chunked
 
-## Sending a message
+More precisely, it is a variant of the CBC mode, with a propagation from one
+block to the next.
+
+With:
+
+- ![p_i][symbol_p-i] the ith plaintext block (length 16 bytes)
+- ![c_i][symbol_c-i] the ith encrypted block (length 16 bytes)
+- ![AES][symbol_aes] the encryption function in ECB mode
+- ![AES][symbol_aes-1] the inverse function, ie the decryption algorithm
+
+![][equation_aes-cbc]
+
+Because of the chaining, xorring each cipher block with a constant &alpha;
+impacts all the plaintext with the same constant:
+
+![][equation_plaintext-tweaking]
+
+This can be easily demonstrated by a mathematical induction, thanks to the
+former equation set.
+
+And it means that we can modify the plaintext without knowing the key!
+
+There is little chance that the random cipher text will produce a full
+Fibonacci sequence after decryption. The only plausible possibility is a
+single padding byte, `OxO1`. So:
+
+Finally! This gives us the last byte of the last block.
+In my case: `b`
+
+## The padding oracle
 
 The server tries to decrypt with a fixed value for a:
 
@@ -35,21 +71,13 @@ HTB{th3_s3crt_A}
 0x4854427b7468335f73336372745f417d
 ```
 
-The first step of the decryption is xorring with a:
 
-```
-ECB-1(x) = b ^ AES-1 (a ^ x)
-```
 
-`a` is updated after each decrypted block, so the input value is actually used
-once, on the first block.
-
-Noting `a` the original IV, `a_` the fixed IV:
-
-```
-b_ = b ^ a ^ a_
-&rarr; b_ ^ a_ = b ^ a
-```
-
-So replacing the first block with `b_ = b ^ a ^ a_` will force the decryption
-using the original `a`.
+[equation_aes-cbc]: maths/equation_aes-cbc.png
+[equation_decrypt-last-byte]: maths/equation_decrypt-last-byte.png
+[equation_invert-secret-iv]: maths/equation_invert-secret-iv.png
+[equation_plaintext-tweaking]: maths/equation_plaintext-tweaking.png
+[symbol_aes]: maths/symbol_aes.png
+[symbol_aes-1]: maths/symbol_aes-1.png
+[symbol_c-i]: maths/symbol_c-i.png
+[symbol_p-i]: maths/symbol_p-i.png
--- hackthebox/crypto/fibopadcci/fibopadcci.py
+++ hackthebox/crypto/fibopadcci/fibopadcci.py
@@ -1,11 +1,56 @@
-import argparse
-
+import socketserver
 from Crypto.Cipher import AES
+import os
+
+# ========================================================================= FIB
+
+fib = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 121, 98, 219, 61]
+
+# ========================================================================= AES
+
+class SuperSecureEncryption: # This should be unbreakable!
+    def __init__(self, key):
+        self.cipher = AES.new(key, AES.MODE_ECB)
+
+    def encrypt(self, data):
+        data = pad(data)
+        
+        a = os.urandom(16).replace(b'\x00', b'\xff') 
+        b = os.urandom(16).replace(b'\x00', b'\xff')
+
+        lb_plain = a
+        lb_cipher = b
+        output = b''
+
+        for block in chunks(data, 16):
+    
+            enc = self.cipher.encrypt(xor(lb_cipher, block))
+            enc = xor(enc, lb_plain)
+            output += enc
+            lb_plain = block
+            lb_cipher = enc
+        return output, a.hex(), b.hex()
+
+    def decrypt(self, data, a, b):
+        lb_plain = a
+        lb_cipher = b
+        output = b''
+        for block in chunks(data, 16):
+            dec = self.cipher.decrypt(xor(block, lb_plain))
+            dec = xor(dec, lb_cipher)
+            output += dec
+            lb_plain = dec
+            lb_cipher = block
+        return output
+        # if checkpad(output):
+        #     return output
+        # else:
+        #     return None
 
 # ============================================================== tcp connection
 
-HOST = 127.0.0.1
-PORT = 1337
+HOST = "127.0.0.1"
+PORT = "1337"
 
 def check_response():
     pass
@@ -32,6 +77,11 @@ def bruteforce():
 
 # =================================================================== utilities
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 def int_to_byte(n: int) -> str:
     __h = str(hex(n % 255))[2:]
     return bytes.fromhex("0" * (2-len(__h)) + __h)
@@ -39,34 +89,55 @@ def int_to_byte(n: int) -> str:
 def xor(a: bytes, b: bytes) -> bytes:
     return bytes([_a ^ _b for _a, _b in zip(a, b)])
 
-def pad(data): #Custom padding, should be fine!
+def pad(data: bytes) -> bytes: #Custom padding, should be fine!
     c = 0
     while len(data) % 16:
         data += int_to_byte(fib[c])
         c += 1
     return data
 
+def checkpad(data: bytes) -> bytes:
+    
+    if len(data) % 16:
+        return 0
+    char = data[-1]
+
+    try:
+        start = fib.index(char)
+    except ValueError:
+        return 0
+    
+    newfib = fib[:start][::-1]
+
+    for i in range(len(newfib)):
+        char = data[-(i+2)]
+        if char != newfib[i]:
+            return 0
+    return 1
+
 # =============================================================== change the IV
 
-def change_plain_iv(
-        ct: bytes,
-        old: bytes,
-        sub: bytes) -> bytes:
-    if len(ct) % 16:
+def mask(
+        message: bytes,
+        pattern: bytes) -> bytes:
+    if len(message) % len(pattern):
         return None
     else:
-        return xor(xor(ct[:16], old), sub) + ct[16:]
+        __mask = pattern * (len(message) // len(pattern))
+        return xor(message, __mask)
 
-# ask for the encrypted flag
-def main():
-    ct = bytes.fromhex("434d6b77c1b4396326d008b2dab8415b")
-    a = bytes.fromhex("a8934508402a306729d2a82d32dea41b")
-    b = bytes.fromhex("b6ba9921ac82c44b5ee20b52345e1076")
+# ============================================================ encryption tests
 
-    flags = bruteforce()
+KEY    = b'16bytesofsupersecretkeyunpadded_'
+A      = bytes.fromhex("98a8ea331fa71a7fdf2824d9ff70460e")
+B      = bytes.fromhex("0afb737c2a61819637c95a3bd96dad87")
+CT     = bytes.fromhex("6caa603e2e74aa50270411cec6d5cb027f75a5b68fecc1403542392c7421617d9e644968c6f43691909e7146422455f4")
+A_     = b'HTB{th3_s3crt_A}'
+AA_    = xor(A, A_)
+B_     = mask(B, AA_)
+CT_    = mask(CT, AA_)
+CIPHER = SuperSecureEncryption(KEY)
 
-    while True:
-        pass
+# ======================================================================== main
 
-if __name__ == '__main__':
-    main()
+print(f'ct:\t{CT.hex()}\nct_:\t{CT_.hex()}\na:\t{A.hex()}\na_:\t{A_.hex()}\nb:\t{B.hex()}\n')
