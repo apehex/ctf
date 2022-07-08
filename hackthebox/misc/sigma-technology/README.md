782bf04b903c4107d4729d964756cca7bb079b2a
apehex <apehex@protonmail.com>
2022-07-08 23:27:11 +0200

    Code the FGSM adversarial attack on NN to misclassify Julius the dog

--- hackthebox/misc/sigma-technology/README.md
+++ hackthebox/misc/sigma-technology/README.md
@@ -19,6 +19,38 @@ The input image is 32 by 32:
 
 ![][julius.png]
 
+The robot is 99.99% sure Juliues is a dog before the attack:
+
+```python
+s = SigmaNet()
+i = Image.open('../images/julius.png').convert('RGB')
+p = s.predict(asarray(i))[0]
+c = np.argmax(p)
+
+print(s.predict_one(asarray(i)))
+# 'dog'
+print(p[c])
+# 0.9998697
+```
+
+## Creating the adversarial image
+
+Tensorflow has a [great tutorial][tf-tuto] 
+
+Verify:
+
+```python
+SIGMA(preprocess(julius))
+SIGMA(preprocess(julius) + 0.2 * perturbations)
+```
+
+```python
+most_significant_coordinates
+# [(20, 12), (14, 18), (14, 21), (15, 24), (16, 18)]
+```
+
 [author-profile]: https://app.hackthebox.com/users/32848
 [julius]: images/julius.png
 [laser-coordinates]: images/laser-coordinates.png
+[sparse-attacks]: https://github.com/fra31/sparse-imperceivable-attacks
+[tf-tuto]: https://www.tensorflow.org/tutorials/generative/adversarial_fgsm
--- hackthebox/misc/sigma-technology/sources/model.py
+++ hackthebox/misc/sigma-technology/sources/model.py
@@ -1,34 +1,86 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+################################################################ interpretation
 
-class SigmaNet:
-    def __init__(self):
-        self.name = 'sigmanet'
-        self.model_filename = 'sigmanet.h5'
-        try:
-            self._model = load_model(self.model_filename)
-            print('Successfully loaded', self.name)
-        except (ImportError, ValueError, OSError):
-            print('Failed to load', self.name)
+CLASS_NAMES = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+DOG_INDEX = 5
 
-    def color_process(self, imgs):
-        if imgs.ndim < 4:
-            imgs = np.array([imgs])
-        imgs = imgs.astype('float32')
-        mean = [125.307, 122.95, 113.865]
-        std = [62.9932, 62.0887, 66.7048]
-        for img in imgs:
-            for i in range(3):
-                img[:, :, i] = (img[:, :, i] - mean[i]) / std[i]
-        return imgs
+######################################################################### model
 
-    def predict(self, img):
-        processed = self.color_process(img)
-        return self._model.predict(processed)
+SIGMA = tf.keras.models.load_model('sigmanet.h5')
 
-    def predict_one(self, img):
-        confidence = self.predict(img)[0]
-        predicted_class = np.argmax(confidence)
-        return class_names[predicted_class]
+################################################################### input image
+
+julius = tf.image.decode_image(tf.io.read_file('../images/julius.png'), channels=3)
+
+################################################################# preprocessing
+
+MEAN_RGB = np.array([125.307, 122.95, 113.865])
+STD_RGB = np.array([62.9932, 62.0887, 66.7048])
+
+def preprocess(img):
+    __img = img
+    for i in range(3):
+        __img[:, :, i] = (__img[:, :, i] - MEAN_RGB[i]) / STD_RGB[i]
+    return np.array([__img])
+
+def preprocess(img):
+    __layer = tf.keras.layers.Normalization(axis=-1, mean=MEAN_RGB, variance=STD_RGB**2)
+    __image = __layer(img)
+    return __image[None, ...]
+
+def postprocess(x):
+    pass
+
+################################################################### __predictions
+
+def predict(x):
+    __confidence = SIGMA(x)[0]
+    __index = np.argmax(__confidence)
+    return __index, __confidence[__index], CLASS_NAMES[__index]
+
+############################################################# adversarial noise
+
+loss = tf.keras.losses.CategoricalCrossentropy()
+
+def create_adversarial_pattern(image, index, model=SIGMA):
+    with tf.GradientTape() as __tape:
+        __tape.watch(image)
+        __prediction = model(image)
+        __loss = loss(index, __prediction)
+
+    # Get the gradients of the loss w.r.t to the input image.
+    return __tape.gradient(__loss, image)
+    # Get the sign of the gradients to create the perturbation
+    # return tf.sign(__gradient)
+
+index = tf.one_hot([DOG_INDEX], len(CLASS_NAMES))
+perturbations = create_adversarial_pattern(preprocess(julius), index, SIGMA)
+
+###############################################  most significant perturbations
+
+def argmax(a, n):
+    __a_flat = a.flatten()
+
+    # Find the indices in the 1D array
+    __i_flat = __a_flat.argsort()[-n:]
+
+    # convert the 1D indices back into coordinates
+    __x, __y = np.unravel_index(__i_flat, a.shape)
+
+    # format as (x, y) tuples
+    return zip(__x, __y)
+
+norm = tf.norm(perturbations[0], axis=-1).numpy() # as numpy array instead of tensor
+most_significant_coordinates = list(argmax(norm, 5))
+# most_significant_perturbations = 
+
+####################################################################### display
+
+plt.figure()
+plt.imshow(julius)
+plt.imshow(perturbations[0])
+# plt.imshow(perturbations[0] * 0.5 + 0.5)  # To change [-1, 1] to [0,1]
+plt.show()
